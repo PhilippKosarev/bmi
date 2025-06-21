375181becbb643cb742079b19681644dba31cec2
PhilippKosarev <philipp.kosarev@gmail.com>
2025-06-21 19:29:58 +0100

    update po files

--- update_po.sh
+++ update_po.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo "Updating pot file."
-xgettext --files-from=po/POTFILES.in --output=po/bmi.pot --join-existing --omit-header
+command
+xgettext --files-from=po/POTFILES.in \
+  --output=po/bmi.pot \
+  --join-existing \
+  --omit-header \
+  --from-code=UTF-8 \
+  --add-comments \
+  --keyword=_ \
+  --keyword=C_:1c,2
+
 po_files=($(find ./po/ -name *.po))
 echo "Updating: ${po_files[@]}"
 for file in ${po_files[@]}; do
